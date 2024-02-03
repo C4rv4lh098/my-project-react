d05bad5d4d0661b80d8dd304d4d97e52b31f9985
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2024-02-03 12:04:59 -0400

    Restart Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import MeuContador from "./MeuContador"
-
 import BuscandoDados from "./components/BuscandoDades"
-
-
-// import MinhaLista from "./components/MinhaLista"
 
 function App() {
 
   return (
     <div>
-      {/* <MeuContador /> */}
-      {/* <h1>Listas no React</h1> */}
-      {/* <MinhaLista /> */}
       <BuscandoDados />
     </div>
   )
